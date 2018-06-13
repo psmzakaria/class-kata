7a97f61ef85183abb3b143b57880e9b8540c1a22
psmzakaria <psmzakaria@gmail.com>
2018-06-13 15:35:03 +0800

    Completed

---
+++ Waiter.js
@@ -0,0 +1,25 @@
+class Waiter {
+  constructor(name) {
+    this.name = name;
+    this.orders = [];
+  }
+  welcomeGuests() {
+    return "welcome! what can i do for you today?";
+  }
+  takeOrder(orders) {
+    this.orders = orders;
+    return orders;
+  }
+  repeatOrder() {
+    return console.log("This is your order", this.orders);
+  }
+}
+
+const waiter1 = new Waiter("Moshi");
+console.log(
+    waiter1.takeOrder(["Chicken Rice", "Chilli Sauce", "Ice Lemon Tea"])
+);
+waiter1.repeatOrder();
+// This is another way to excecute.
+// console.log(waiter1.name);
+// const order1 = ["Chicken Rice", "Chilli Sauce", "Ice Lemon Tea"];
